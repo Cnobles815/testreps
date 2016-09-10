7d4b0641d35246e95c93506495ebaeb7e79c4ded
Cnobles815 <Cnobles815@gmail.com>
2016-09-09 20:13:46 -0400

    Add files via upload

---
+++ Television.java
@@ -0,0 +1,26 @@
+package nobles.christopher.ZCWLivingRoom;
+
+/**
+ * Created by christophernobles on 9/9/16.
+ */
+public class Television {
+
+    private Integer volume;
+    private Integer channel;
+
+    public Television() {
+        volume = 0;
+        channel = 0;
+    }
+
+    public Integer getVolume () {
+
+        return 0;
+
+    }
+
+    public void setVolume(Integer amount) {
+
+    }
+
+}
